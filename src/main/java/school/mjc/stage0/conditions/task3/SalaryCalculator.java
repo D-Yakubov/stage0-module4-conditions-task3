8207573109344c8c594c13756a3f98c4d32721fe
Dastonbek <yakubovdastonbek@gmail.com>
2022-12-29 07:53:11 +0500

    i am sorry

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public void calculateSalary(float salary) {
+        if (salary < 0){
+            System.out.println("wrong input!");
+        }else if (salary <= 10_000){
+            System.out.println(salary-salary*15/100);
+        }else if (10_000 < salary && salary <= 20_000){
+            System.out.println(salary-salary*18/100);
+        }else if (salary > 20_000){
+            System.out.println(salary-salary*20/100);
+        }
     }
 }
